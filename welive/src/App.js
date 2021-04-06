cb28ba00657face93de5db00c39218d85580eb08
harry <luhanyao@gmail.com>
2021-04-06 22:04:33 +1200

    welive project day1

---
+++ welive/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Header from './components/Header'
+import Featured from './components/Featured'
+import './resources/styles.css'
+
+export default function App() {
+  return (
+    <div>
+      <Header/>
+      <Featured/>
+    </div>
+  );
+}
+
